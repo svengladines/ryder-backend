86588e3637d197ac74913429fc97ff4e5f51cf51
Sven Gladines <sven.gladines@gmail.com>
2021-05-05 23:30:23 +0200

    trying to login via Strava...

--- src/main/java/be/occam/ryder/RyderApplication.java
+++ src/main/java/be/occam/ryder/RyderApplication.java
@@ -3,10 +3,23 @@ package be.occam.ryder;
 import org.springframework.boot.Banner;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.authentication.HttpStatusEntryPoint;
 
 @SpringBootApplication
-@Enableoua
-public class RyderApplication {
+public class RyderApplication extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+    	// @formatter:off
+        http
+        	.authorizeRequests()
+        	.anyRequest().authenticated()
+        	.and()
+        	.oauth2Login();
+    }
 	
 	public static void main(String[] args) {
         new SpringApplicationBuilder()
